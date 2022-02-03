97fa85a936d98725c88f15fc854168bd1ec6d4e0
Henry Tijerina III <henrytijerina@gmail.com>
2022-02-03 15:37:57 -0600

    Change Footer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -8,6 +9,8 @@ export default function Home() {
       <Head>
         <title>GC Invitational</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       </Head>
 
       <main>
